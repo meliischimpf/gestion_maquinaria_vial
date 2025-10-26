a4a82bc99641af5122d2e54d9ea3345420a5ae45
meliischimpf <melischimpf10@gmail.com>
2025-10-26 15:19:35 -0300

    deploy fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
-
 set -e
 
 echo "--- 1. Instalar dependencias de Composer (Composer V2 compatible) ---"
 composer install --no-dev --optimize-autoloader
-php artisan cache:clear
-php artisan config:cache
-php artisan view:cache
 
 echo "--- 2. Instalar y compilar assets de NPM (frontend) ---"
 npm install
 npm run build
 
-echo "--- 3. Ejecutar Migraciones de Base de Datos ---"
+echo "--- 3. Ejecutar Migraciones y Optimizar la Aplicación ---"
 php artisan migrate --force
+php artisan cache:clear
+php artisan config:cache
+php artisan view:cache
 
 echo "--- 4. Arreglar permisos ---"
 chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
