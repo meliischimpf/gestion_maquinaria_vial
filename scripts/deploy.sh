dcf71db81c077a449d0f30f76085c359d2c82b04
meliischimpf <melischimpf10@gmail.com>
2025-11-02 00:47:46 -0300

    se agregan seeders a produccion

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,15 +3,21 @@
 set -e
 
 cd /var/www/html
-echo "--- 1. Ejecutar Migraciones y Optimizar la Aplicación ---"
-php artisan migrate:fresh --seed
 
+echo "--- 1. Limpiar caché ---"
 php artisan cache:clear
+php artisan config:clear
+php artisan view:clear
+
+echo "--- 2. Migrar y sembrar ---"
+php artisan migrate:fresh --seed --force
+
+echo "--- 3. Finalmente optimizar ---"
 php artisan config:cache
 php artisan view:cache
-php artisan optimize:clear
+php artisan optimize
 
-echo "--- 2. Arreglar permisos finales ---"
+echo "--- 4. Arreglar permisos finales ---"
 chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
 chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
 
